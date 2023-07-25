3460ee2cb8dd90d770118c24ee20e8093c83115b
Saleh Eissa <saleheissa@Salehs-MBP.attlocal.net>
2023-07-25 16:09:44 -0500

    change html

--- script.js
+++ script.js
@@ -1,15 +1,1 @@
-// Global variables
-var secretNumber;
-var numGuesses;
 
-// Function to start a new game
-function newGame() {
-  // Generate a new secret number
-  secretNumber = Math.floor(Math.random() * 100) + 1;
-  // Reset the number of guesses
-  numGuesses = 0;
-  // Reset the feedback text
-  document.getElementById("feedback").textContent = "";
-  // Reset the number of guesses text
-  document.getElementById("numGuesses").textContent = "";
-}
